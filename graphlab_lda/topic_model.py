81726d454d1c063d8a120fc8a109103b68c93460
000 <a000@yktxs-Air.lan>
2016-12-06 11:30:56 +0800

    update

--- graphlab_lda/topic_model.py
+++ graphlab_lda/topic_model.py
@@ -70,8 +70,10 @@ data_sframe_dir = real_dir_path + '/data_sframe'
 
 g_channel_model_dict = {}
 
+import datetime
 data_dir = real_dir_path + '/data/'
-def create_model_proc(csv_file):
+model_dir = real_dir_path + 'models'
+def create_model_proc(csv_file, model_save_dir=None):
     if not os.path.exists(data_sframe_dir):
         os.mkdir(data_sframe_dir)
 
@@ -80,14 +82,31 @@ def create_model_proc(csv_file):
     docs = docs.dict_trim_by_keys(gl.text_analytics.stopwords(), exclude=True)
     model = gl.topic_model.create(docs, num_iterations=100, num_topics=50, verbose=True)
     g_channel_model_dict[csv_file] = model
+    #save model
+    if model_save_dir:
+        model.save(model_save_dir+'/'+csv_file)
 
 
 def create_models():
+    model_create_time = datetime.datetime.now()
+    model_create_time.strftime('%Y-%m-%d %H:%M:%S')
+    model_path = real_dir_path + '/' + model_create_time
+    if not os.path.exists(model_path):
+        os.mkdir(model_path)
+
     from topic_model_doc_process import channel_for_topic
     for chanl in channel_for_topic:
-        create_model_proc(chanl)
+        create_model_proc(chanl, model_path)
     print 'create models finished!!'
 
+
+def load_models(models_dir):
+    global g_channel_model_dict, model_dir
+    if len(g_channel_model_dict) != 0:
+        g_channel_model_dict.clear()
+    models_files = os.listdir(models_dir)
+    for mf in models_files:
+        g_channel_model_dict[models_files] = gl.load_model(model_dir + '/' + mf)
 
 def lda_predict(nid):
     global g_channel_model_dict
@@ -101,25 +120,30 @@ def lda_predict(nid):
     docs = gl.SFrame(data={'X1':ws})
     docs = gl.text_analytics.count_words(docs['X1'])
     docs = docs.dict_trim_by_keys(gl.text_analytics.stopwords(), exclude=True)
-    sf = g_channel_model_dict[chanl_name].get_topics(num_words=20, output_type='topic_words')
+    sf = g_channel_model_dict[chanl_name].get_topics(num_words=20,
+                                                     output_type='topic_words')
 
     #预测得分最高的topic
-    pred = g_channel_model_dict[chanl_name].predict(docs)
-    print pred
-    print '%s' % str(sf[pred[0]]['words']).decode('string_escape')
+    #pred = g_channel_model_dict[chanl_name].predict(docs)
+    #print pred
+    #print '%s' % str(sf[pred[0]]['words']).decode('string_escape')
 
     print '=================================='
-    pred2 = g_channel_model_dict[chanl_name].predict(docs, output_type='probability')
+    pred2 = g_channel_model_dict[chanl_name].predict(docs,
+                                                     output_type='probability')
     num_dict = {}
     num = 0
     for i in pred2[0]:
-        num_dict[pred2[0][num]] = num
+        num_dict[i] = num
         num += 1
-    probility = sorted(num_dict.items(), key=lambda d:d[0], reverse=True)
+    probility = sorted(num_dict.items(), key=lambda d: d[0], reverse=True)
     i = 0
-    while i < 3 and i <len(probility) and probility[i][0] > 0.1:
+    res = []
+    while i < 3 and i < len(probility) and probility[i][0] > 0.1:
         print '%s' % str(sf[probility[i][1]]['words']).decode('string_escape')
+        res.append(sf[probility[i][1]]['words'])
         i += 1
+    return res
 
 
 
--- lda_service.py
+++ lda_service.py
@@ -4,13 +4,50 @@
 # @Brief   : 
 # @File    : lda_service.py
 # @Software: PyCharm Community Edition
+import json
+import sys
+
+import tornado
+from tornado import web
+
 from graphlab_lda import topic_model_doc_process
 from graphlab_lda import topic_model
 
 #topic_model_doc_process.coll_news_for_channles()
 
 #topic_model.create_model('体育')
-topic_model.create_models()
-topic_model.lda_predict(6897344)
+#topic_model.create_models()
+#topic_model.lda_predict(6897344)
+
+class CreateModels(tornado.web.RequestHandler):
+    def get(self):
+        topic_model.create_models()
 
 
+class PredictOnNid(tornado.web.RequestHandler):
+    def get(self):
+        nid = int(self.get_argument('nid'))
+        res = topic_model.lda_predict(nid)
+        self.write(json.dumps(res))
+
+
+class LoadModels(tornado.web.RequestHandler):
+    def get(self):
+        models_dir = self.get_argument('dir')
+        topic_model.load_models(models_dir)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            ("/topic_model/create_models", CreateModels),
+            ("/topic_model/predict_on_nid", PredictOnNid),
+            ("/topic_model/load_models", LoadModels),
+        ]
+
+if __name__ == '__main__':
+    pass
+    port = sys.argv[1]
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
