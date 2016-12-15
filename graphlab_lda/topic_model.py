a5c06e8f3aeb90e81a7a0856530b192468ee5f01
000 <a000@yktxs-Air.lan>
2016-12-15 16:23:20 +0800

    update

--- graphlab_lda/topic_model.py
+++ graphlab_lda/topic_model.py
@@ -75,7 +75,7 @@ def load_models(models_dir):
     models_files = os.listdir(models_dir)
     for mf in models_files:
         print '    load ' + mf
-        g_channel_model_dict[models_files] = gl.load_model(model_dir + mf)
+        g_channel_model_dict[models_files] = gl.load_model(model_dir + mf +'/objects.bin')
 
 
 def get_newest_model_dir():
