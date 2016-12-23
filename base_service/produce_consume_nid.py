2db701054e6fc0436eb73d3945ceab812abf804c
000 <a000@yktxs-MacBook-Air.local>
2016-12-23 15:51:52 +0800

    update

--- base_service/produce_consume_nid.py
+++ base_service/produce_consume_nid.py
@@ -8,15 +8,15 @@
 import requests
 def push_nid_to_queue(nid):
     #lda_model处理
-    lda_url = "http://120.55.88.11:9986/topic_model/predict_news_topic"
+    lda_url = "http://127.0.0.1:9986/topic_model/predict_news_topic"
     data = {}
     data['nid'] = nid
     requests.get(url=lda_url, params=data)
 
     #去除广告服务
-    ads_url = "http://120.55.88.11/news_process/RemoveAdsOnnid"
-    ads_data = {}
-    ads_data['nid'] = nid
-    requests.get(url=ads_url, params=ads_data)
+    #ads_url = "http://120.55.88.11/news_process/RemoveAdsOnnid"
+    #ads_data = {}
+    #ads_data['nid'] = nid
+    #requests.get(url=ads_url, params=ads_data)
 
 
