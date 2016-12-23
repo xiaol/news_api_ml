481887fed839facd2bb45909179e41181c837efa
000 <a000@yktxs-MacBook-Air.local>
2016-12-23 14:03:19 +0800

    update

--- graphlab_lda/timed_task.py
+++ graphlab_lda/timed_task.py
@@ -16,7 +16,6 @@ handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-conn, cursor = get_postgredb()
 
 #click_sql = "select uid, nid, ctime from newsrecommendclick where ctime > now() - INTERVAL '5 minute'"
 click_sql = "select c.uid, c.nid, c.ctime from newsrecommendclick c \
@@ -26,13 +25,16 @@ where cname in ({0}) and c.ctime > now() - INTERVAL '5 minute'"
 
 channels = ', '.join("\'" + ch+"\'" for ch in channel_for_topic)
 def get_clicks_5s():
+    conn, cursor = get_postgredb()
     cursor.execute(click_sql.format(channels))
     rows = cursor.fetchall()
-    logger.info('-----------------------------')
+    #logger.info('-----------------------------')
     for r in rows:
         ctime_str = r[2].strftime('%Y-%m-%d %H:%M:%S')
         s = str(r[0]) + ' ' + str(r[1]) + ctime_str
         logger.info(s)
         produce_user_click(r[0], r[1], ctime_str)
+    cursor.close()
+    conn.close()
 
 
