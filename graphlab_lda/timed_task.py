00f4ae24afe4289f031885d8fe3b8f79bedfeb48
000 <a000@yktxs-MacBook-Air.local>
2016-12-23 12:07:45 +0800

    update

--- graphlab_lda/timed_task.py
+++ graphlab_lda/timed_task.py
@@ -7,12 +7,19 @@
 
 from util.doc_process import get_postgredb
 from redis_lda import produce_user_click
+from topic_model_doc_process import channel_for_topic
 
 conn, cursor = get_postgredb()
 
-click_sql = "select uid, nid, ctime from newsrecommendclick where ctime > now() - INTERVAL '5 minute'"
+#click_sql = "select uid, nid, ctime from newsrecommendclick where ctime > now() - INTERVAL '5 minute'"
+click_sql = "select nl.nid from newsrecommendclick c \
+inner join newslist_v2 nl  on c.nid=nl.nid \
+INNER JOIN channellist_v2 cl on nl.chid = cl.id \
+where cname in ({0}) and c.ctime > now() - INTERVAL '5 minute'"
+
+channels = ', '.join("\'" + ch+"\'" for ch in channel_for_topic)
 def get_clicks_5s():
-    cursor.execute(click_sql)
+    cursor.execute(click_sql.format(channels))
     rows = cursor.fetchall()
     for r in rows:
         ctime_str = r[2].strftime('%Y-%m-%d %H:%M:%S')
