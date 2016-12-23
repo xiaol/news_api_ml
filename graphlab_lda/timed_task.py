7b12b90bfab8b212848ab4bdbe301b76fd2793aa
000 <a000@yktxs-MacBook-Air.local>
2016-12-23 13:34:40 +0800

    updte

--- graphlab_lda/timed_task.py
+++ graphlab_lda/timed_task.py
@@ -8,7 +8,14 @@
 from util.doc_process import get_postgredb
 from redis_lda import produce_user_click
 from topic_model_doc_process import channel_for_topic
-
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('log_lda.txt')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 conn, cursor = get_postgredb()
 
 #click_sql = "select uid, nid, ctime from newsrecommendclick where ctime > now() - INTERVAL '5 minute'"
@@ -21,8 +28,11 @@ channels = ', '.join("\'" + ch+"\'" for ch in channel_for_topic)
 def get_clicks_5s():
     cursor.execute(click_sql.format(channels))
     rows = cursor.fetchall()
+    logger.info('-----------------------------')
     for r in rows:
         ctime_str = r[2].strftime('%Y-%m-%d %H:%M:%S')
+        s = str(r[0]) + ' ' + str(r[1]) + ctime_str
+        logger.info(s)
         produce_user_click(r[0], r[1], ctime_str)
 
 
