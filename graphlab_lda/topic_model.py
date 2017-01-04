b1ba400cb9dde2b486df369dfd207f17fd793a00
000 <a000@yktxs-MacBook-Air.local>
2017-01-04 12:40:13 +0800

    update

--- graphlab_lda/topic_model.py
+++ graphlab_lda/topic_model.py
@@ -455,9 +455,8 @@ def predict_click(click_info):
 def show_topics():
     chname = '体育'
     newest_dir = get_newest_model_dir()
-    p = os.path.join(real_dir_path, 'models', newest_dir, chname)
-    print p
     model = gl.load_model(os.path.join(real_dir_path, 'models', newest_dir, chname))
+    print type(model)
     model.show()
 
 
