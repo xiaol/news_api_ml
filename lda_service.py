e0df82cf7556086e1b34bd574b5680adc5015fbc
000 <a000@yktxs-MacBook-Air.local>
2017-01-04 14:29:22 +0800

    update

--- lda_service.py
+++ lda_service.py
@@ -173,6 +173,8 @@ if __name__ == '__main__':
         #topic_model.load_newest_models()
         redis_lda.consume_user_click()
     elif port == 9100: #可视化
+        http_server = tornado.httpserver.HTTPServer(EmptyApp())
+        http_server.listen(port) #同时提供手工处理端口
         topic_model.show_topics()
     tornado.ioloop.IOLoop.instance().start()
 
