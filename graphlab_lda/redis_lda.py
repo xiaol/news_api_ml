d3e3735adbf73171e05697d534237045bef196e1
000 <a000@yktxs-MacBook-Air.local>
2017-01-13 11:13:23 +0800

    update

--- graphlab_lda/redis_lda.py
+++ graphlab_lda/redis_lda.py
@@ -4,10 +4,13 @@
 # @Brief   : 
 # @File    : redis_lda.py
 # @Software: PyCharm Community Edition
+from graphlab_lda import topic_model
+from graphlab_kmeans import kmeans
 from redis import Redis
 import traceback
 redis_inst = Redis(host='localhost', port=6379)
 nid_queue = 'nid_queue_for_lda'
+
 
 def produce_nid(nid):
     global redis_inst
@@ -31,7 +34,6 @@ def consume_nid(num):
                 #topic_model.lda_predict_and_save(nid_list)
                 topic_model.predict_topic_nids(nid_list)
                 #for kmeans
-                from graphlab_kmeans import kmeans
                 kmeans.kmeans_predict(nid_list)
                 n = 0
                 del nid_list[:]
@@ -48,7 +50,6 @@ def produce_user_click(uid, nid, ctime):
     redis_inst.lpush(user_click_queue, json.dumps([uid, nid, ctime]))
 
 
-from graphlab_lda import topic_model
 #消费用户点击行为
 def consume_user_click():
     global redis_inst
@@ -59,36 +60,11 @@ def consume_user_click():
             nid = data[1]
             ctime = data[2]
             print 'consum ' + str(uid) + ' ' + str(nid) + ' ' + 'begin'
-            #topic_model.predict_user_topic_core(uid, nid, ctime)
+            #for topic model
             topic_model.predict_click((uid, nid, ctime))
             #for kmeans
-            from graphlab_kmeans import kmeans
             kmeans.predict_click((uid, nid, ctime))
         except :
             traceback.print_exc()
             continue
-
-
-
-#import topic_model_v2
-#def consume_user_clicks(num):
-#    global redis_inst
-#    n = 0
-#    click_list = []
-#    while True:
-#        try:
-#            #data[0]--uid, data[1]--nid, data[2]--ctime
-#            data = json.loads(redis_inst.brpop(user_click_queue)[1])
-#            if num:
-#                n += 1
-#                click_list.append((data[0], data[1], data[2]))
-#                if n >= num:
-#                    topic_model_v2.predict_user_topic_core(click_list)
-#                    n = 0
-#                    del click_list[:]
-#
-#        except :
-#            traceback.print_exc()
-            continue
-
 
--- graphlab_lda/timed_task.py
+++ graphlab_lda/timed_task.py
@@ -9,20 +9,22 @@ from util.doc_process import get_postgredb
 from redis_lda import produce_user_click
 from topic_model_doc_process import channel_for_topic
 
+#定义取用户点击的循环周期
+period = 10
 #click_sql = "select uid, nid, ctime from newsrecommendclick where ctime > now() - INTERVAL '5 minute'"
 click_sql = "select c.uid, c.nid, c.ctime from newsrecommendclick c \
 inner join newslist_v2 nl  on c.nid=nl.nid \
 INNER JOIN channellist_v2 cl on nl.chid = cl.id \
-where cname in ({0}) and c.ctime > now() - INTERVAL '5 minute' \
-and \
-(c.uid, c.nid) not in ( \
-select c2.uid, c2.nid from newsrecommendclick c2 \
-where c2.ctime < now() - interval '5 minute' and c2.ctime > now() - INTERVAL '3 day') "
+where cname in ({0}) and c.ctime > now() - INTERVAL '{1} second' "
+#and \
+#(c.uid, c.nid) not in ( \
+#select c2.uid, c2.nid from newsrecommendclick c2 \
+#where c2.ctime < now() - interval ' second' and c2.ctime > now() - INTERVAL '1 day') "
 
 channels = ', '.join("\'" + ch+"\'" for ch in channel_for_topic)
 def get_clicks_5m():
     conn, cursor = get_postgredb()
-    cursor.execute(click_sql.format(channels))
+    cursor.execute(click_sql.format(channels, period))
     rows = cursor.fetchall()
     for r in rows:
         ctime_str = r[2].strftime('%Y-%m-%d %H:%M:%S')
