5afabf7bfe138bab587252807817ca981be66c22
000 <a000@yktxs-MacBook-Air.local>
2017-02-16 12:51:27 +0800

    update

--- multi_viewpoint/sentence_hash.py
+++ multi_viewpoint/sentence_hash.py
@@ -141,6 +141,8 @@ def coll_sentence_hash():
         for r in rows:
             all_set.add(r[0])
         need_to_cal_set = all_set - exist_set
+        if len(need_to_cal_set) == 0:
+            continue
         pool.apply_async(cal_process, args=(need_to_cal_set,))
         i += len(rows)
         if i > 100:
