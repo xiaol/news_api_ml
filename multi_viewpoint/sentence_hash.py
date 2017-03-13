d09c952e42b531fad0ebcc532a280750cfbbf671
liuleigit <liulei_git@sina.com>
2017-03-13 22:38:37 +0800

    update

--- multi_viewpoint/sentence_hash.py
+++ multi_viewpoint/sentence_hash.py
@@ -179,7 +179,8 @@ multo_vp_insert_sql = "insert into news_multi_vp (nid1, sentence1, nid2, sentenc
 ################################################################################
 #@brief: 计算子进程
 ################################################################################
-def cal_process(nid_set, log, same_t=3):
+def cal_process(nid_set, log=None, same_t=3):
+    log = logger_9966
     log.info('there are {} news to calulate'.format(len(nid_set)))
     nid_sents_dict, nid_para_links_dict, nid_pname_dict = get_nids_sentences(nid_set)
     same_dict = get_relate_same_news(nid_set)
@@ -349,7 +350,7 @@ def coll_sentence_hash_time(nid_list):
     small_list = [nid_list[i:i + 20] for i in range(0, len(nid_list), 20)]
     pool = Pool(20)
     for nid_set in small_list:
-        pool.apply_async(cal_process, args=(set(nid_set), logger_9965, 3))
+        pool.apply_async(cal_process, args=(set(nid_set), None, 3))
 
     pool.close()
     pool.join()
@@ -385,7 +386,7 @@ def coll_sentence_hash():
         need_to_cal_set = all_set - exist_set
         if len(need_to_cal_set) == 0:
             continue
-        pool.apply_async(cal_process, args=(need_to_cal_set, logger_9966, 3))
+        pool.apply_async(cal_process, args=(need_to_cal_set, None, 3))
 
     pool.close()
     pool.join()
