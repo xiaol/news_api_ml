d48281c70cd42803125fb1bce1c65b57fe37a3b0
liuleigit <liulei_git@sina.com>
2017-03-16 20:59:52 +0800

    update

--- graphlab_lda/data_process.py
+++ graphlab_lda/data_process.py
@@ -33,32 +33,30 @@ a."chid"=c."id" ORDER BY nid desc LIMIT %s'
 
 def coll_news_proc(save_dir, chnl, doc_num_per_chnl, doc_min_len):
     logger.info('    start to collect {} ......'.format(chnl))
-    print '0000000'
+    '''
     f = open(os.path.join(save_dir, chnl), 'w') #定义频道文件
     conn, cursor = doc_process.get_postgredb_query()
     cursor.execute(channle_sql, [chnl, doc_num_per_chnl])
     rows = cursor.fetchall()
-    print '00000000'
     for row in rows:
         title = row[0]
         content_list = row[1]
         txt = ''
-        print '11111111'
         for content in content_list:
             if 'txt' in content.keys():
                 txt += content['txt'].encode('utf-8')
         total_txt = title + txt
-        print '22222222'
         total_list = doc_process.filter_html_stopwords_pos(total_txt, remove_num=True, remove_single_word=True)
         if len(total_list) < doc_min_len:  #字数太少则丢弃
             continue
         #根据tfidf进行二次筛选
         total_list = doc_process.jieba_extract_keywords(' '.join(total_list), min(50, len(total_list)/5))
-        print '33333333'
         f.write(' '.join(total_list).encode('utf-8') + '\n')
         del content_list
         del total_list
     conn.close()
+    '''
+    print 'fffffffff' + chnl
 
 
 class DocProcess(object):
