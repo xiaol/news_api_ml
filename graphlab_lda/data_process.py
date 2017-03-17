8cd0793f613c5dd335a689dd2bcf429ae61318b1
liuleigit <liulei_git@sina.com>
2017-03-17 10:15:26 +0800

    update

--- graphlab_lda/data_process.py
+++ graphlab_lda/data_process.py
@@ -21,7 +21,7 @@ doc_min_len = 100
 #需要
 channel_for_topic = ['科技', '外媒', '社会', '财经', '体育', '汽车', '国际', '时尚', '探索', '科学',
                      '娱乐', '养生', '育儿', '股票', '互联网', '美食', '健康', '影视', '军事', '历史',
-                     '故事', '旅游', '美文', '萌宠', '游戏', '点集', '自媒体', '奇闻', '奇点']
+                     '故事', '旅游', '美文', '萌宠', '游戏', '点集', '自媒体', '奇闻']
 excluded_chnl = ['美女', '视频', '趣图', '搞笑']
 
 
@@ -36,9 +36,6 @@ def coll_news_proc(save_dir, chnl, doc_num_per_chnl, doc_min_len):
     try:
         logger.info('    start to collect {} ......'.format(chnl))
         f = open(os.path.join(save_dir, chnl), 'w+') #定义频道文件
-        f.write('aaaaaaaaaaa')
-        print '^^^^^^^^^^^^^^^^^^^^^^^^'
-        print os.path.join(save_dir, chnl)
         conn, cursor = doc_process.get_postgredb_query()
         cursor.execute(channle_sql, (chnl, doc_num_per_chnl))
         rows = cursor.fetchall()
@@ -61,10 +58,8 @@ def coll_news_proc(save_dir, chnl, doc_num_per_chnl, doc_min_len):
         cursor.close()
         conn.close()
         f.close()
-        print 'fffffffff' + chnl
     except:
-        print 'eeeeeeeeeeeeeeee'
-        print traceback.format_exc()
+        logger.exception(traceback.format_exc())
 
 doc_num_per_chnl = doc_num_per_chnl
 doc_min_len = doc_min_len
@@ -117,30 +112,25 @@ class DocProcess(object):
     def coll_news_handler(self):
         logger.info("coll_news_handler begin ...!")
         t0 = datetime.datetime.now()
-        import multiprocessing as mp
         from multiprocessing import Pool
         pool = Pool(20)
         from util.doc_process import join_file
-        procs = []
         chnl_file = []
         for chanl in channel_for_topic:
             chnl_file.append(os.path.join(self.save_dir, chanl))
             pool.apply_async(coll_news_proc, args=(self.save_dir, chanl, self.doc_num_per_chnl, self.doc_min_len))
-            #coll_proc = mp.Process(target=self.coll_news_proc, args=(chanl,))
-            #coll_proc.start()
-            #procs.append(coll_proc)
         pool.close()
         pool.join()
-        #for i in procs:
-        #    i.join()
         join_file(chnl_file, os.path.join(self.save_dir, '/data.txt'))
         t1 = datetime.datetime.now()
         logger.info("coll_news_handler finished!, it takes {}s".format((t1 - t0).total_seconds()))
 
 
 def coll_news():
-    coll_news_handler(save_dir, doc_num_per_chnl, doc_min_len)
-    print '-----------finish------'
+    #coll_news_handler(save_dir, doc_num_per_chnl, doc_min_len)
+    dp = DocProcess(doc_num_per_chnl, doc_min_len)
+    dp.coll_news_handler()
+    print 'finished!!'
 
 
 
