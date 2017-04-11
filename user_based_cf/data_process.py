609c8c5180a3203c8dd9a5a37b17d25597379334
liuleigit <liulei_git@sina.com>
2017-04-11 17:44:13 +0800

    update

--- user_based_cf/data_process.py
+++ user_based_cf/data_process.py
@@ -181,6 +181,8 @@ def get_user_topic_similarity(users, topics, props):
     W = dict()
     for u, related_users in C.items():
         for v, cuv in related_users.items():
+            if (user_invert_dict[v] in W) and (user_invert_dict[u] in W[user_invert_dict[v]]):
+                continue
             if user_invert_dict[u] not in W:
                 W[user_invert_dict[u]] = dict()
             if user_invert_dict[v] not in W[user_invert_dict[u]]:
