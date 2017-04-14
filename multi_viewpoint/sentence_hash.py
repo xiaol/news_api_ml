564a11728711b75ac7b432c15d91bf1c6b637b1e
liuleigit <liulei_git@sina.com>
2017-04-14 17:29:03 +0800

    update

--- multi_viewpoint/sentence_hash.py
+++ multi_viewpoint/sentence_hash.py
@@ -353,11 +353,11 @@ def coll_sentence_hash_time(nid_list):
 
 
 cal_sql = "select nid from newslist_v2 limit %s offset %s"
-cal_sql2 ='SELECT a.nid \
+cal_sql2 ="SELECT a.nid \
 FROM newslist_v2 a \
-RIGHT OUTER JOIN (select * from channellist_v2 where "cname" not in %s) c \
+RIGHT OUTER JOIN (select * from channellist_v2 where cname not in %s) c \
 ON \
-a."chid" =c."id" where (a.ctime > now() - interval "2 day") and a.state=0 LIMIT %s offset %s'
+a.'chid' =c.'id' where (a.ctime > now() - interval '2 day') and a.state=0 LIMIT %s offset %s"
 ignore_cname = ("美女", "帅哥", "搞笑", "趣图", "视频")
 
 def coll_sentence_hash():
