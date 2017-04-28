d403a4099f0be45bf55c3612018ab6229cfa89f4
liuleigit <liulei_git@sina.com>
2017-04-28 11:40:09 +0800

    update

--- multi_viewpoint/subject_queue.py
+++ multi_viewpoint/subject_queue.py
@@ -9,18 +9,18 @@ import json
 import traceback
 
 
-redis_inst = Redis(host='localhost', port=6378)
+sub_redis = Redis(host='localhost', port=6379)
 subject_queue = 'subject_queue' #专题队列
 
 def product_subject(sub_nids):
-    redis_inst.lpush(subject_queue, json.dumps(sub_nids))
+    sub_redis.lpush(subject_queue, json.dumps(sub_nids))
 
 def consume_subject():
     from subject import generate_subject
-    global redis_inst
+    global sub_redis
     while True:
         try:
-            nids = json.loads(redis_inst.brpop(subject_queue)[1])
+            nids = json.loads(sub_redis.brpop(subject_queue)[1])
             print nids
             generate_subject(nids)
 
